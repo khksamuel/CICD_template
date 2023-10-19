d55759cf5a3a18bcdc4204cb48877d5f3d693e40
Aryan Moradi <aryan.moradi@ozu.edu.tr>
2023-10-20 00:43:19 +1030

    updated executable

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Update system
+sudo apt-get update
+
+# Install PostgreSQL
+sudo apt-get install -y postgresql
+
+# Install Node.js and npm
+sudo apt-get install -y nodejs npm
+
+# Install Grafana
+wget https://dl.grafana.com/oss/release/grafana_8.2.3_amd64.deb
+sudo dpkg -i grafana_8.2.3_amd64.deb
+
 # Clone the repository
 git clone https://github.com/khksamuel/CICD_template.git
 
