6af67b27b4a9451676b0a712d3d954ced2d4f8e0
Aryan Moradi <aryan.moradi@ozu.edu.tr>
2023-10-20 00:38:13 +1030

    project executable

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/khksamuel/CICD_template.git
+
+# Navigate to the repository
+cd CICD_template
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Install Node.js dependencies
+npm install
